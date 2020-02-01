7b8719bc2ed955b324fac73571f3ea2ebd15cffe
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-01 01:29:26 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,9 +84,12 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }
+        :yukihiro_matsumoto => {
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        }
      }
      
-    :yukihiro_matsumoto => {}
     
 end
 
