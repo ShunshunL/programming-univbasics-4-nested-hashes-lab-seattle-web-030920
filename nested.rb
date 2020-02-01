f8af1a85d471efaeb9e1544097f5fba49157e34c
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-01 01:18:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,14 @@ def hopper
           :languages => ["C"]
         }
      }
-
+  	programmer_hash = 
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        }
+ 		}
+  
 
 end
 
